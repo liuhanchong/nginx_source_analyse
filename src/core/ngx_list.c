03409209320d56ef099e01a873d0f5677a4d6f58
liuhanchong <18256916415@163.com>
2015-09-22 17:31:31 +0800

    ngx_list 链表学习
    1.链表的原理比较简单，但是注意，其实现使用多个数组组成链表。虽然我们用时候感觉不出来。

--- src/core/ngx_list.c
+++ src/core/ngx_list.c
@@ -14,6 +14,7 @@ ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
     ngx_list_t  *list;
 
+	/*分配内存*/
     list = ngx_palloc(pool, sizeof(ngx_list_t));
     if (list == NULL) {
         return NULL;
@@ -39,23 +40,28 @@ ngx_list_push(ngx_list_t *l)
 
         /* the last part is full, allocate a new list part */
 
+		//分配一个新的链表
         last = ngx_palloc(l->pool, sizeof(ngx_list_part_t));
         if (last == NULL) {
             return NULL;
         }
 
+		//为新的链表分配空间
         last->elts = ngx_palloc(l->pool, l->nalloc * l->size);
         if (last->elts == NULL) {
             return NULL;
         }
 
+		//初始化链表
         last->nelts = 0;
         last->next = NULL;
 
+		//将新的链表加入到队列
         l->last->next = last;
-        l->last = last;
+        l->last = last;/*将最后一个指向新的链表*/
     }
 
+	//将新的元素插入链表
     elt = (char *) last->elts + l->size * last->nelts;
     last->nelts++;
 
--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -16,18 +16,18 @@
 typedef struct ngx_list_part_s  ngx_list_part_t;
 
 struct ngx_list_part_s {
-    void             *elts;
-    ngx_uint_t        nelts;
-    ngx_list_part_t  *next;
+    void             *elts;/*元素为一个数组*/
+    ngx_uint_t        nelts;/*已经装了的元素个数*/
+    ngx_list_part_t  *next;/*下一个*/
 };
 
 
 typedef struct {
-    ngx_list_part_t  *last;
-    ngx_list_part_t   part;
-    size_t            size;
-    ngx_uint_t        nalloc;
-    ngx_pool_t       *pool;
+    ngx_list_part_t  *last;/*最后一个链表*/
+    ngx_list_part_t   part;/*链表*/
+    size_t            size;/*元素大小*/
+    ngx_uint_t        nalloc;/*分配的元素个数*/
+    ngx_pool_t       *pool;/*分配的内存池*/
 } ngx_list_t;
 
 
@@ -36,11 +36,13 @@ ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);
 static ngx_inline ngx_int_t
 ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
+	/*为链表分配*/
     list->part.elts = ngx_palloc(pool, n * size);
     if (list->part.elts == NULL) {
         return NGX_ERROR;
     }
 
+	//初始化数据
     list->part.nelts = 0;
     list->part.next = NULL;
     list->last = &list->part;
