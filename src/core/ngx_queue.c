eab18446ae91390b5b15c96c02dd8e5c24641f52
liuhanchong <18256916415@163.com>
2015-09-23 13:45:00 +0800

    ngx_queue 队列学习

--- src/core/ngx_queue.c
+++ src/core/ngx_queue.c
@@ -19,23 +19,31 @@ ngx_queue_middle(ngx_queue_t *queue)
 {
     ngx_queue_t  *middle, *next;
 
+	//获取到头
     middle = ngx_queue_head(queue);
 
+	//如果存在一个元素那就是middle 如果不存在 那就是NULL
     if (middle == ngx_queue_last(queue)) {
         return middle;
     }
 
+	//获取到头
     next = ngx_queue_head(queue);
 
+	//next 每次循环走两步 当 next走到头 middle正好走到中间
     for ( ;; ) {
+		//获取到下一个节点
         middle = ngx_queue_next(middle);
 
+		//获取到下一个节点
         next = ngx_queue_next(next);
 
+		//如果next是最后一个 
         if (next == ngx_queue_last(queue)) {
             return middle;
         }
 
+		//走两回
         next = ngx_queue_next(next);
 
         if (next == ngx_queue_last(queue)) {
@@ -53,19 +61,26 @@ ngx_queue_sort(ngx_queue_t *queue,
 {
     ngx_queue_t  *q, *prev, *next;
 
+	//获取首个元素
     q = ngx_queue_head(queue);
 
+	//空链表
     if (q == ngx_queue_last(queue)) {
         return;
     }
 
+	//排序
     for (q = ngx_queue_next(q); q != ngx_queue_sentinel(queue); q = next) {
 
+		//获取前一个
         prev = ngx_queue_prev(q);
+		//获取后一个
         next = ngx_queue_next(q);
 
+		//删除Q 
         ngx_queue_remove(q);
 
+		//和前别元素进行比较
         do {
             if (cmp(prev, q) <= 0) {
                 break;
@@ -75,6 +90,7 @@ ngx_queue_sort(ngx_queue_t *queue,
 
         } while (prev != ngx_queue_sentinel(queue));
 
+		//将Q插入到比他小的元素前边队列中
         ngx_queue_insert_after(prev, q);
     }
 }
--- src/core/ngx_queue.h
+++ src/core/ngx_queue.h
@@ -21,15 +21,17 @@ struct ngx_queue_s {
 };
 
 
+//初始化链表
 #define ngx_queue_init(q)                                                     \
-    (q)->prev = q;                                                            \
+    (q)->prev = q;                                                            \ 
     (q)->next = q
 
 
+//表明链表第一个元素不做为元素，第二个元素才是第一个
 #define ngx_queue_empty(h)                                                    \
     (h == (h)->prev)
 
-
+//在头部插入节点
 #define ngx_queue_insert_head(h, x)                                           \
     (x)->next = (h)->next;                                                    \
     (x)->next->prev = x;                                                      \
@@ -39,7 +41,7 @@ struct ngx_queue_s {
 
 #define ngx_queue_insert_after   ngx_queue_insert_head
 
-
+//双向链表操作
 #define ngx_queue_insert_tail(h, x)                                           \
     (x)->prev = (h)->prev;                                                    \
     (x)->prev->next = x;                                                      \
@@ -47,22 +49,25 @@ struct ngx_queue_s {
     (h)->prev = x
 
 
+	//获取第一个元素
 #define ngx_queue_head(h)                                                     \
     (h)->next
 
 
+	//获取最后一个元素
 #define ngx_queue_last(h)                                                     \
     (h)->prev
 
-
+//哨兵
 #define ngx_queue_sentinel(h)                                                 \
     (h)
 
-
+//回去元素的下一个
 #define ngx_queue_next(q)                                                     \
     (q)->next
 
 
+	//获取元素前一个
 #define ngx_queue_prev(q)                                                     \
     (q)->prev
 
@@ -77,13 +82,14 @@ struct ngx_queue_s {
 
 #else
 
+	//删除一个元素
 #define ngx_queue_remove(x)                                                   \
     (x)->next->prev = (x)->prev;                                              \
     (x)->prev->next = (x)->next
 
 #endif
 
-
+//分离开链表 以Q为分界点 目前分析 
 #define ngx_queue_split(h, q, n)                                              \
     (n)->prev = (h)->prev;                                                    \
     (n)->prev->next = n;                                                      \
@@ -92,18 +98,19 @@ struct ngx_queue_s {
     (h)->prev->next = h;                                                      \
     (q)->prev = n;
 
-
+//增加一个新的链表，将两个链表合并 n 为另一个链表的哨兵
 #define ngx_queue_add(h, n)                                                   \
     (h)->prev->next = (n)->next;                                              \
     (n)->next->prev = (h)->prev;                                              \
     (h)->prev = (n)->prev;                                                    \
     (h)->prev->next = h;
 
-
+//获取到数据
 #define ngx_queue_data(q, type, link)                                         \
     (type *) ((u_char *) q - offsetof(type, link))
 
 
+//获取中间的元素
 ngx_queue_t *ngx_queue_middle(ngx_queue_t *queue);
 void ngx_queue_sort(ngx_queue_t *queue,
     ngx_int_t (*cmp)(const ngx_queue_t *, const ngx_queue_t *));
